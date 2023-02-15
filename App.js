d2b1679ced8084d31e8c945884f41d13c1f60bf7
AkshayBodakhe <abodakhe38@gmail.com>
2023-02-15 14:23:39 +0530

    redux

--- App.js
+++ App.js
@@ -9,13 +9,16 @@ import MealsOverviewScreen from "./screens/MealsOverviewScreen";
 import MealDetaillScreen from "./screens/MealDetaillScreen";
 import "react-native-gesture-handler";
 import FavoritesScreen from "./screens/FavoritesScreen";
-import FavoritesContextProvider from "./store/context/favorites-context";
+// import FavoritesContextProvider from "./store/context/favorites-context";
+import { Provider } from "react-redux";
+import {store} from './store/redux/store'
 
 const Stack = createNativeStackNavigator();
 const Drawer = createDrawerNavigator();
 
 function DrawerNavigator() {
   return (
+    
     <Drawer.Navigator
       screenOptions={{
         headerStyle: { backgroundColor: "#351401" },
@@ -39,6 +42,7 @@ function DrawerNavigator() {
         drawerIcon : ({color,size}) => <Ionicons name="star" color={color} size={size}/>
       }}
       />
+    
     </Drawer.Navigator>
   );
 }
@@ -47,7 +51,8 @@ export default function App() {
   return (
     <>
       <StatusBar style="light" />
-      <FavoritesContextProvider>
+      {/* <FavoritesContextProvider> */}
+      <Provider store={store}>
       <NavigationContainer>
         <Stack.Navigator
           initialRouteName="MealsCategories"
@@ -74,7 +79,8 @@ export default function App() {
           />
         </Stack.Navigator>
       </NavigationContainer>
-    </FavoritesContextProvider>
+      </Provider>
+    {/* </FavoritesContextProvider> */}
     </>
   );
 }
--- screens/MealDetaillScreen.js
+++ screens/MealDetaillScreen.js
@@ -5,23 +5,32 @@ import MealDetails from "../components/MealDetails";
 import Subtitle from "../components/MealDetail/Subtitle";
 import List from "../components/MealDetail/List";
 import IconButton from "../components/IconButton";
-import { FavoritesContext } from "../store/context/favorites-context";
+// import { FavoritesContext } from "../store/context/favorites-context";
+import { useDispatch, useSelector } from "react-redux";
+
+import { addFavorite,removeFavorite } from "../store/redux/favorites";
 
 const MealDetaillScreen = ({ route , navigation }) => {
-  
-  const favoritesMealsCtx = useContext(FavoritesContext);
+  // const favoritesMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMealIds = useSelector((state)=>state.favoriteMeals.ids);
+  const dispatch = useDispatch();
   
   const mealId = route.params.mealId;
   const selectedMeal = MEALS.find((meal) => meal.id === mealId);
 
-  const mealIsFavorite = favoritesMealsCtx.ids.includes(mealId);
+
+  // const mealIsFavorite = favoritesMealsCtx.ids.includes(mealId);
+  const mealIsFavorite = favoriteMealIds.includes(mealId);
 
    function changeFavoriteStatusHandler(){
      console.log("pressed!");
      if(mealIsFavorite){
-      favoritesMealsCtx.removeFavorite(mealId);
+      // favoritesMealsCtx.removeFavorite(mealId);
+      dispatch(removeFavorite({id:mealId}))
      }else{
-      favoritesMealsCtx.addFavorite(mealId)
+      // favoritesMealsCtx.addFavorite(mealId)
+      dispatch(addFavorite({id:mealId}))
      }
    }
 
